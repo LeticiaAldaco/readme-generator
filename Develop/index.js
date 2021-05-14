3825a3708bc898525841405cb858713aab77d8f4
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-05-14 18:36:58 -0500

    initial set up

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,33 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //description, installation instructions, usage information, contribution guidelines, and test instructions
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of this project?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Describe the use of your project',
+},
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+console.log(answers.title, answers.description);
+    })
+}
 
 // Function call to initialize app
 init();
