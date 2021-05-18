016ddffd2d64b588da71c3ad4bf2d879aac2064c
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-05-17 23:02:57 -0500

    comepleted license badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,7 @@ for (i = 0; i < licenses.length; i++) {
     }
   }
 }
-
+`
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
